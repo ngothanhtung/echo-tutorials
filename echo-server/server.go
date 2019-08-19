6bf24a1d4e0d87fe310db568a55dcd2a5793247a
Ngo Thanh Tung <tung.ngo@outlook.com>
2019-08-20 01:48:08 +0700

    Update Go-Server

---
+++ echo-server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/show", show)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team: "+team+", member: "+member)
+}
